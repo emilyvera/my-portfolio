12431862fc2f952ca2b2a5faa28ad8a1b3541c6e
emilyvera <evera5@illinois.edu>
2020-06-03 18:55:10 +0000

    Fix about-me block to be responsive on different screens

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,19 +14,72 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.gson.Gson;
+import java.util.ArrayList;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.sps.servlets.Comment;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/data")
+@WebServlet("/contact-me")
 public class DataServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
-  }
+    private ArrayList<Comment> comments;
+
+    @Override
+    public void init() {
+        comments = new ArrayList<>();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json;");
+        String json = new Gson().toJson(comments);
+        response.getWriter().println(json);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Get the input from the form.
+        Comment c = getComment(request);
+
+        comments.add(c);
+
+        // Make sure it went through
+        System.out.println(comments.get(0).getName());
+        System.out.println(comments.get(0).getEmail());
+        System.out.println(comments.get(0).getSubject());
+        System.out.println(comments.get(0).getMessage());
+
+        Entity commentEntity = new Entity("Comment");
+        commentEntity.setProperty("name", comments.get(0).getName());
+        commentEntity.setProperty("email", comments.get(0).getEmail());
+        commentEntity.setProperty("subject", comments.get(0).getSubject());
+        commentEntity.setProperty("message", comments.get(0).getMessage());
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(commentEntity);
+
+        // Redirect back to the HTML page.
+        response.sendRedirect("/index.html");
+    }
+
+    private Comment getComment(HttpServletRequest request) {
+        // Get the input from the form.
+        String name = request.getParameter("name-input");
+        String email = request.getParameter("email-input");
+        String subject = request.getParameter("subject-input");
+        String message = request.getParameter("message-input");
+
+        // Construct comment object.
+        Comment c = new Comment(name, email, subject, message);      
+
+        return c;
+    }
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,20 +12,51 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['I am 20 years old!', 'My favorite color is pink!', 
-      'I am in Phi Sigma Sigma!', 'I used to work at John Deere!',
-      'I am outdoorsy!', 'My favorite race is the 5k!',
-      'I have done other Google programs before!'];
+// /**
+//  * Adds a random greeting to the page.
+//  */
+// function addRandomGreeting() {
+//   const greetings =
+//       ['I am 20 years old!', 'My favorite color is pink!', 
+//       'I am in Phi Sigma Sigma!', 'I used to work at John Deere!',
+//       'I am outdoorsy!', 'My favorite race is the 5k!',
+//       'I have done other Google programs before!'];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+//   // Pick a random greeting.
+//   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+//   // Add it to the page.
+//   const greetingContainer = document.getElementById('greeting-container');
+//   greetingContainer.innerText = greeting;
+// }
+
+// async function getText() {
+//   const response = await fetch('/data');
+//   const txt = await response.text();
+//   document.getElementById('txt-container').innerText = txt;
+// }
+
+function getMessages() {
+        console.log("Made it here 1");
+  fetch('/data').then(response => response.json()).then((messages) => {
+    // messages is an object, not a string, so we have to
+    // reference its fields to create HTML content
+
+    console.log("Made it here 2");
+
+    const messagesElement = document.getElementById('messages-container');
+    messagesElement.innerHTML = '';
+    messagesElement.appendChild(
+        createListElement('1: ' + messages[0]));
+    messagesElement.appendChild(
+        createListElement('2: ' + messages[1]));
+    messagesElement.appendChild(
+        createListElement('3: ' + messages[2]));
+  });
+}
+
+function createListElement(text) {
+  const liElement = document.createElement('li');
+  liElement.innerText = text;
+  return liElement;
 }
